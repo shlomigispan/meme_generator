aaea8c94c48c6fc700a3e688e6e5e2a082db03f7
shlomigispan <shlomigispan@gmail.com>
2025-04-15 09:11:05 +0300

    meme gen proj

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "/src/components/Header";
+import Main from "/src/components/Main";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import trollFace from "/src/assets/troll-face.png";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src={trollFace} />
+      <h1>Meme Generator</h1>
+    </header>
+  );
+}
